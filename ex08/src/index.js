98c8e61747ffe1acd1a0b282a85a1ca925a6a08c
Smajo K <smajo.kulovic2512@gmail.com>
2021-03-11 05:41:26 +0100

    little math

---
+++ ex08/src/index.js
@@ -0,0 +1,15 @@
+function main(numOne, numTwo) {
+    var sum = 15 + 8
+
+
+
+
+    // Only change below this line
+
+    // Only change above this line
+    return sum;
+
+}
+
+console.log(main(15,8));
+module.exports = main;
