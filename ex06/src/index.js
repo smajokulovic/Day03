13e682d3bed7e731d5f5bc1bae37e77e2ba54c78
Smajo K <smajo.kulovic2512@gmail.com>
2021-03-11 03:45:08 +0100

    my name is John

---
+++ ex06/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+    
+    var x = 7;
+    var y = 12;
+    var z = "My anme is ";
+    
+    // Only change code below this
+    // Only change code above this
+    x = x + 2;
+    y = y + 4;
+    z = z + "John!"
+    return [x,y,z];
+}
+
+console.log(main());
+module.exports = main;
