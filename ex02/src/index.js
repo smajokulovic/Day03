5325f4609ca970c380431842a88e5ed8eb0906b2
Smajo K <smajo.kulovic2512@gmail.com>
2021-03-11 02:44:20 +0100

    Comment

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    // Only change code below this line
+    // Only change code above this line
+}
+console.log(main());
+module.exports = main;
+// Idemoooooooo
+/* Zeljo;
+Zeljo;
+Zeljo;
+zeljo */
