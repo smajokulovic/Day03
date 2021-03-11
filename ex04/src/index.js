53ec9e9cd798e1787bacd83358b0c63a1ef4a4e1
Smajo K <smajo.kulovic2512@gmail.com>
2021-03-11 03:22:32 +0100

    Assigment Operator

---
+++ ex04/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    
+    var x;
+    x = 10;
+    var y;
+    y = x;
+    // Only change code below this
+    // Only change code above this
+    return [x, y];
+}
+console.log(main());
+module.exports = main;
+
+
+
