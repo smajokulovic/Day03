a396e8e048b20150352754fe41744147c15e2470
Smajo K <smajo.kulovic2512@gmail.com>
2021-03-11 03:34:20 +0100

    Variables

---
+++ ex05/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    
+    var x = 8;
+    
+    // Only change code below this
+    // Only change code above this
+    return x;
+    
+}
+console.log(main());
+module.exports = main;
