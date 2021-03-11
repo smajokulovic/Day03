9b4a928cd1340c9936ba328d83650a5da4b4f748
Smajo K <smajo.kulovic2512@gmail.com>
2021-03-11 05:55:49 +0100

    love math

---
+++ ex09/src/inbox.js
@@ -0,0 +1,12 @@
+function main (numOne, numTwo) {
+
+    var difference = 25 - 8;
+    // Only change below this line
+
+    // Only change above this line
+    return difference
+}
+
+console.log(main(25,8))
+module.exports = main;
+
