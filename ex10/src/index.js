ebb1bdd2224c547e34f74c9278a66c901fc7ecb5
Smajo K <smajo.kulovic2512@gmail.com>
2021-03-11 05:59:17 +0100

    in love whit math

---
+++ ex10/src/index.js
@@ -0,0 +1,11 @@
+function main (numOne, numTwo) {
+
+    var product = 25 * 8;
+    // Only change below this line
+
+    // Only change above this line
+    return product;
+}
+
+console.log(main(25,8))
+module.exports = main;
